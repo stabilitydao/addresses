a178116cc33c86126e57c2ca0c2b8a8d781ea7e8
a17 <m@a17.name>
2021-11-23 01:11:17 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 export const MAINNET = 1
 export const ROPSTEN = 3
 export const RINKEBY = 4
+export const GOERLI = 5
+export const KOVAN = 42
+// export const OPTIMISTIC_KOVAN = 69
+// export const MUMBAI = 80001
 
 export default {
   [MAINNET]: {
@@ -14,5 +18,13 @@ export default {
   [RINKEBY]: {
     token: '0x7D68ECf1762bA27120d0f98BFcFa0da67eb15860',
     devFund: '0x62706332c976f92bBd5F099fB8D4717951eC00a4',
+  },
+  [GOERLI]: {
+    token: '0x4D7D07196E24D15d8E5B97216aeDCf4518b23A66',
+    devFund: '0x90AfD7eEE756c85Aa9800d33Fda674618ddECFbf',
+  },
+  [KOVAN]: {
+    token: '0x108aBca337e88a9fc1DE96b0ec323f476b35cD44',
+    devFund: '0x90AfD7eEE756c85Aa9800d33Fda674618ddECFbf',
   },
 }
